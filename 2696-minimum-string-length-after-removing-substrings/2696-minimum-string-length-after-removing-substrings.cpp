8c7f08c702524feaf07ed394b2538371a3513c22
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-06-26 02:58:32 +0300

    Time: 0 ms (100%), Space: 10 MB (98.04%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int minLength(string s) {
 
-
         int last = 0;
         for (int i = 0; i < s.size(); i++) {
 
-            if (s[i] == 'B'  && last && s[last-1] == 'A') {
+            if (s[i] == 'B' && last && s[last - 1] == 'A') {
+                last--;
+            } else if (s[i] == 'D' && last && s[last - 1] == 'C') {
                 last--;
             }
-            else if (s[i] == 'D'  && last && s[last-1] == 'C') {
-               last--;
-            }
 
-            else{
-                s[last] = s[i] ;
+            else {
+                s[last] = s[i];
                 last++;
-                }
+            }
         }
         return last;
     }
