6c2e91af2d6efd4058f1056f36d74cec614eccfc
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-07-20 01:34:51 +0300

    Time: 4 ms (0.94%), Space: 8.1 MB (17.9%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n < 0) return 0;
-        // bool flag = 0;
-        // for (int i = 31; i >= 0; --i) {
-        //     if (flag == 1 and ((n >> i) & 1) == 1) return 0;
-        //     if (((n >> i) & 1) == 1)flag = 1; 
-        // }
-        // return flag;
-
-        bitset<32> bits(n);
-        if(bits.count() != 1 ) return 0;
-        return 1;
+        vector<int> vec(32,0) ;
+        long sum = 1;
+        /*for(int i = 0; i < vec.size(); i++){
+            vec[i] = sum;
+            if (i == 31) sum  = sum * 2  - 1;
+            else sum *= 2;
+            cout <<i<<":"<< vec[i] << " ";
+        } */
+        for(int i = 0; i < 32; i++){
+            if (n == sum) return 1;
+            else sum *= 2;
+        }
+        return 0;
     }
 };
