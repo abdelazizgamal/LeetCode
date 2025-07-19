4d9cc9def1bb27dfe376cb05e1f26ae8adcdcbad
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-07-20 00:26:39 +0300

    Time: 2 ms (56.76%), Space: 9.7 MB (48.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() == t.size()){
-            sort(s.begin(),s.end());
-            sort(t.begin(),t.end());
-            if(s == t) return 1;
+        vector<int> Arr(26,0);
+        cout<< 'z'-'a' << endl;
+        for(int i = 0; i < s.size(); i++){
+            Arr[s[i]-'a'] += 1;
         }
-        return 0;
+
+        for(int i = 0; i < t.size(); i++){
+            Arr[t[i]-'a'] -= 1;
+        }
+        for(int i = 0; i < Arr.size(); i++){
+            if(Arr[i] != 0) return 0;
+        }
+        return 1;
         
     }
 };
