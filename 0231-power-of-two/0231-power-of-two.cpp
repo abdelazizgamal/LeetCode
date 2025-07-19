113401bcc176cc645f3cecb6ccff93bf5d1590c9
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-07-20 01:09:38 +0300

    Time: 0 ms (100%), Space: 7.8 MB (83.63%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         bool flag = 0;
-        for (int i = 30; i >= 0; --i) {
-            if (((n >> 31) & 1) == 0){
+        if (((n >> 31) & 1) == 0)
+            for (int i = 30; i >= 0; --i) {
                 if (flag == 1 and ((n >> i) & 1) == 1) return 0;
-                if (((n >> i) & 1) == 1)flag = !flag;
-
-            } 
-            
-        }
+                if (((n >> i) & 1) == 1)flag = 1; 
+            }
         return flag;
     }
 };
