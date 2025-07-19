0bb38842e3b46390f7f8820c2f3968e8f39cc772
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-07-20 00:01:03 +0300

    Time: 4 ms (39.29%), Space: 9.6 MB (90.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() == t.size()){
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+            if(s == t) return 1;
+        }
+        return 0;
+        
+    }
+};
