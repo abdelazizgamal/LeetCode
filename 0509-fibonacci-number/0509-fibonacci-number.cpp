acf291c0c37f1ed919d029d0ed8b27977fef264c
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-08-11 22:41:57 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
-    int fib(int n) {
-        int sum = 0;
-        if (n == 0) return  0;
-        if (n == 1) return 1;
-        sum = fib(n - 1) + fib(n - 2);
-        return sum;
+     int fib(int n) {
+    //     int sum = 0;
+    //     if (n == 0) return  0;
+    //     if (n == 1) return 1;
+    //     sum = fib(n - 1) + fib(n - 2);
+    //     return sum;
+    int arr[31] = { 0,1 };
+    int sum = 0;
+
+    for (int i = 2; i <= 30; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
     }
+    
+    return arr[n -1] + arr[n - 2];
+    }
+
 };
