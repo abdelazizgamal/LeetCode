e433e49bfd4aeefb1aa987a55b3ff659c7339076
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-08-11 22:52:47 +0300

    Time: 0 ms (100%), Space: 7.7 MB (64.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -8,11 +8,11 @@ public:
     //     return sum;
     int arr[31] = { 0,1 };
     int sum = 0;
-
+    if(n == 0 || n == 1) return arr[n];
+    
     for (int i = 2; i <= 30; i++) {
         arr[i] = arr[i - 1] + arr[i - 2];
     }
-    
     return arr[n -1] + arr[n - 2];
     }
 
