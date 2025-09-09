3be8fa71688236e6816415c5692efc732fc266e5
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-09-09 11:02:58 +0300

    Time: 436 ms (75.15%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+with del_table as(
+    select id,email , ROW_NUMBER() OVER(partition by email order by id ) as num
+    from person
+)
+
+delete from del_table
+where num > 1
