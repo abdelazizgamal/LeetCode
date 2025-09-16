8252f4b048104030c35a9002fcbade244f6bd0ca
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-09-16 14:10:23 +0300

    Time: 2445 ms (40.75%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,9 +1,15 @@
 /* Write your T-SQL query statement below */
-with year_sales as(
-    select product_id, year ,quantity,price, 
-        rank() over(partition by product_id order by year asc) as rnk
-    from Sales
-)
-select product_id , year as first_year , quantity, price
-from year_sales
-where rnk = 1
+-- WITH CTE 
+-- with year_sales as(
+--     select product_id, year ,quantity,price, 
+--         rank() over(partition by product_id order by year asc) as rnk
+--     from Sales
+-- )
+-- select product_id , year as first_year , quantity, price
+-- from year_sales
+-- where rnk = 1
+-----------------------------------------------------------
+select s1.product_id , s1.year as first_year , s1.quantity, s1.price
+from sales s1
+where year = (select min(year) from sales 
+                where product_id = s1.product_id)
