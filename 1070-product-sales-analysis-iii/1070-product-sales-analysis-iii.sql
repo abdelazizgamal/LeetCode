be52b195a830758367080da1780c80a7507ffeef
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-09-16 13:59:21 +0300

    Time: 2263 ms (62.82%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+with year_sales as(
+    select product_id, year ,quantity,price, 
+        rank() over(partition by product_id order by year asc) as rnk
+    from Sales
+)
+select product_id , year as first_year , quantity, price
+from year_sales
+where rnk = 1
