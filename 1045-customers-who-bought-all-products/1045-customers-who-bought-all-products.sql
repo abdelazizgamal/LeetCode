17c85235177aced36819a5f057c564acdb34e561
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-09-18 23:36:20 +0300

    Time: 2311 ms (5.87%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,19 @@
 /* Write your T-SQL query statement below */
-select customer_id 
-from Customer C
-join Product p on c.product_key = p.product_key 
-group by customer_id
-having count(distinct c.product_key) = (select count(product_key) from Product)
+-- select customer_id 
+-- from Customer C
+-- join Product p on c.product_key = p.product_key 
+-- group by customer_id
+-- having count(distinct c.product_key) = (select count(product_key) from Product)
+
+-- select customer_id 
+-- from (select customer_id, count(distinct product_key)as cnt
+--        from Customer C), Product p
+-- where cnt > count(p.product_key )
+with temp as(select customer_id, count(distinct product_key)as cnt
+       from Customer C
+       group by customer_id)
+
+select customer_id  
+from temp t
+where cnt = (select count(product_key) from Product)
+
