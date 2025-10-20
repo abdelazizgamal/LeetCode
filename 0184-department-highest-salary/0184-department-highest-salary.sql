e660b265ec23714e369df0496c65ddbe776eefa1
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-10-20 09:56:29 +0300

    Time: 662 ms (11.05%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,9 +1,19 @@
 /* Write your T-SQL query statement below */
-WITH emp AS (
+/*WITH emp AS (
     SELECT id, name, salary, departmentId,
            RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
     FROM Employee
 )
 SELECT d.name  as Department, e.name as Employee , salary as Salary 
 FROM emp e, Department d
-WHERE rnk = 1 and departmentId =  d.id  ;
+WHERE rnk = 1 and departmentId =  d.id  ;*/
+
+
+
+
+
+select d.name as Department, e.name as Employee, salary
+from Employee e
+join Department d on departmentId = d.id
+where salary = (select max(salary) from Employee e2
+                where e2.departmentId  = e.departmentId )
