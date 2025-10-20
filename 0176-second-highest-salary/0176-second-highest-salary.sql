259f8ee2c0e901fb853de64d7549aea5e3ef0133
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-10-20 11:10:27 +0300

    Time: 418 ms (33.55%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,11 @@
 /* Write your T-SQL query statement below */
-with temp as(
-   select salary, DENSE_RANK() over(order by salary desc) as rnk
-    from Employee
-)
-select max(CASE WHEN rnk = 2 THEN salary ELSE NULL END) as SecondHighestSalary
-from temp
+-- with temp as(
+--    select salary, DENSE_RANK() over(order by salary desc) as rnk
+--     from Employee
+-- )
+-- select max(CASE WHEN rnk = 2 THEN salary ELSE NULL END) as SecondHighestSalary
+-- from temp
+-----------------------------------
+select max(salary) as SecondHighestSalary 
+from employee
+where salary < (select max(salary) from employee)
