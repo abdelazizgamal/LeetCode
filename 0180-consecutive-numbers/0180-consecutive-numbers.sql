0e40f3fa7916b294b658b673b0bd9e34f527cf98
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2025-11-02 07:53:09 +0200

    Time: 613 ms (34.61%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,6 +1,15 @@
-/* Write your T-SQL query statement below */
-select distinct (num )as ConsecutiveNums
-from(select num from Logs l1
-        where num = (select num from logs l2
-                    where l2.id = l1.id + 1 and num = ((select num from logs l3
-                                    where l3.id = l1.id + 2 and num = num )))) as temp
+-- /* Write your T-SQL query statement below */
+-- select distinct (num )as ConsecutiveNums
+-- from(select num from Logs l1
+--         where num = (select num from logs l2
+--                     where l2.id = l1.id + 1 and num = ((select num from logs l3
+--                                     where l3.id = l1.id + 2 and num = num )))) as temp
+
+with lag_lead as(
+    select num , lag =lAG(num) OVER(ORDER BY id), 
+	   lead =LEAD(num) OVER(ORDER BY id)
+    from logs
+)
+SELECT distinct(num) as ConsecutiveNums 
+FROM lag_lead
+where  num = lag and num = lead
